3fb5d6aa420c2a6e5033c6b45c54b9eb39690945
eric <eric355402@yahoo.com>
2020-05-04 17:51:19 -0500

    mvp complete

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -1,18 +1,37 @@
 const request = require("supertest");
 const server = require("../api/server.js");
 
-describe("POST /register", () => {
-  it("Should save user to database", async (done) => {
-    const res = await request(server).post("/api/auth/register").send({
-      username: "dummy",
-      password: "dummy",
-    });
-    expect(res.status).toBe(201);
-    done();
+const db = require("../database/dbConfig");
+
+describe("test register and login endpoints", () => {
+  beforeAll(async () => {
+    await db("users").truncate();
   });
-  it("should return res.type text/html", async (done) => {
-    const res = await request(server).post("/api/auth/register");
-    expect(res.type).toBe("text/html");
-    done();
+  const user = { username: "eric", password: "baker" };
+  //test register endpoint
+  describe("POST to /register", () => {
+    it("Should save user to database", async (done) => {
+      const res = await request(server).post("/api/auth/register").send(user);
+      expect(res.status).toBe(201);
+      done();
+    });
+    it("should return res.type text/html", async (done) => {
+      const res = await request(server).post("/api/auth/register");
+      expect(res.type).toBe("text/html");
+      done();
+    });
+  });
+  //test login endpoint
+  describe("should test login endpoint", async () => {
+    it("should return return res.status(200) when registration happens");
+    const res = await request(server).post("/api/auth/login").send(user);
+    expect(res.status).toBe(200);
+    it("should return a 401 credentials are not correct", async (done) => {
+      const res = await request(server).post("/api/auth/login").send({
+        username: "eri9c",
+        password: "987654",
+      });
+      expect(res.status).toBe(401);
+    });
   });
 });
