a9e6772d1df5f86be84ce8e167d3bb3818a8ca50
eric <eric355402@yahoo.com>
2020-05-03 19:54:43 -0500

    implemented cookies, auth and login complete, jokes routes complete also

--- api/server.js
+++ api/server.js
@@ -1,18 +1,40 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const knexSessionStore = require("connect-session-knex")(session);
 
-const authenticate = require('../auth/authenticate-middleware.js');
-const authRouter = require('../auth/auth-router.js');
-const jokesRouter = require('../jokes/jokes-router.js');
+const authenticate = require("../auth/authenticate-middleware.js");
+const authRouter = require("../auth/auth-router.js");
+const jokesRouter = require("../jokes/jokes-router.js");
 
 const server = express();
 
+const sessionConfig = {
+  name: "chocolate-chip",
+  secret: "myspeshulsecret",
+  cookie: {
+    maxAge: 3600 * 1000,
+    secure: false, // should be true in production
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+
+  store: new knexSessionStore({
+    knex: require("../database/dbConfig"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 3600 * 1000,
+  }),
+};
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
-server.use('/api/auth', authRouter);
-server.use('/api/jokes', authenticate, jokesRouter);
+server.use("/api/auth", authRouter);
+server.use("/api/jokes", authenticate, jokesRouter);
 
 module.exports = server;
